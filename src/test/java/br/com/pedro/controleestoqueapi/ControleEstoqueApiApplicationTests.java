542be9cec566bb75b9059df149a106824c783295
phxofc <phdfrocha99@gmail.com>
2024-03-13 19:24:08 -0300

    criando projeto

---
+++ src/test/java/br/com/pedro/controleestoqueapi/ControleEstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.controleestoqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
